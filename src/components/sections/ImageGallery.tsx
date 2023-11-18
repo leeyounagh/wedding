9b807dfee04dd46a72510b902c369bbd5493c6fb
[IoT1]이수연 <dudgk0216@gmail.com>
2023-11-19 01:03:02 +0900

    이미지갤러리 구현

---
+++ src/components/sections/ImageGallery.tsx
@@ -0,0 +1,46 @@
+import classNames from 'classnames/bind'
+import Section from '../shared/Section'
+import styles from './ImageGallery.module.scss'
+import ImageViewer from '../ImageViewer'
+import { useState } from 'react'
+
+const cx = classNames.bind(styles)
+
+function ImageGallery({ images }: { images: string[] }) {
+  const [selectedIdx, setSelectedIdx] = useState(-1)
+
+  const open = selectedIdx !== -1
+
+  const handleSelectedImage = (idx: number) => {
+    setSelectedIdx(idx)
+  }
+
+  const handleClose = () => {
+    setSelectedIdx(-1)
+  }
+  return (
+    <Section title="사진첩">
+      <ul className={cx('wrap-images')}>
+        {images.map((src, idx) => (
+          <li
+            key={idx}
+            className={cx('wrap-image')}
+            onClick={() => {
+              handleSelectedImage(idx)
+            }}
+          >
+            <img src={src} alt="이미지" />
+          </li>
+        ))}
+      </ul>
+      <ImageViewer
+        images={images}
+        open={open}
+        selectedIdx={selectedIdx}
+        onClose={handleClose}
+      />
+    </Section>
+  )
+}
+
+export default ImageGallery
--- src/components/sections/Video.tsx
+++ src/components/sections/Video.tsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import Section from '../shared/Section'
 import className from 'classnames/bind'
-import styles from './Heading.module.scss'
+import styles from './Video.module.scss'
 
 const cx = className.bind(styles)
 
 function Video() {
-  return <Section>Heading</Section>
+  return (
+    <Section className={cx('container')}>
+      <video
+        autoPlay={true}
+        muted={true}
+        loop={true}
+        poster="/assets/poster.jpg"
+      >
+        <source src="/assets/원본.mp4" type="video/mp4"></source>
+      </video>
+    </Section>
+  )
 }
 
 export default Video
